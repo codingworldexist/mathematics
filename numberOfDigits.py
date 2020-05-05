b68e2e6ef3bf43ee5ed6b48ddf6193d47fedcaf9
codingworldexist <64868676+codingworldexist@users.noreply.github.com>
2020-05-06 02:18:48 +0530

    Update numberOfDigits.py

--- numberOfDigits.py
+++ numberOfDigits.py
@@ -1,18 +1,3 @@
-"""
-Therefore, we can say that:
-10K-1 <= N < 10K
-Applying base-10 logarithm to both sides in the above equation, we get:
-K-1 <= log10(N) < K.
-
-or, K - 1 + 1 <= log10(N) + 1 < K + 1
-or, K <= log10(N) + 1 < K + 1
-Therefore,
-K = floor(log10(N) + 1)
-
-"""
-
-
-
 #  number of digits in N = log10(N) + 1.
 from math import log,floor
 
